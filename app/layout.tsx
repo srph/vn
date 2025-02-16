554b1b742e6b550dac9762f0e5541f6e296bdb04
Kier Borromeo <seraphipod@gmail.com>
2025-02-16 14:22:10 +0800

    chore: favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -51,10 +51,7 @@ export default function RootLayout({
           name="viewport"
           content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no"
         />
-        <link
-          rel="icon"
-          href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 32 32%22><rect width=%2232%22 height=%2232%22 fill=%22%23DA251D%22/><polygon points=%2216,4 20.5,17 8,11 24,11 11.5,17%22 fill=%22%23FFFF00%22/></svg>"
-        />
+        <link rel="icon" href="/favicon.png" />
       </head>
       <body className="dark:bg-neutral-900 dark:text-neutral-100">
         {children}
